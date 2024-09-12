a22ff91f2f5ab3f15e4c691ffd7f289762c535be
rohat güneş <rohatgns44@gmail.com>
2024-09-12 15:27:34 +1000

    Add project files.

---
+++ Pratik-AspNetCoreEmpty/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pratik_AspNetCoreEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pratik-AspNetCoreEmpty/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// servisleri ekleylim bvuraya
+// mc servis kaydı yapacagız
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// http istek  hattını yapılandıralım 
+app.UseStaticFiles();
+
+// Routing yapılandırması da asagıdaki gibi olacak 
+
+//  şimdi de Varsayılan routing tanımlayılım 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
